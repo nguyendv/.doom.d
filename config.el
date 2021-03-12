958e17c2315b94fc0458945411abc6289f25e5dd
Dave Nguyen <dvnguyen@amazon.com>
2021-03-12 08:18:52 -0800

    Enable lsp, js, python, editorconfig

--- config.el
+++ config.el
@@ -29,11 +29,15 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dropbox/org")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+
+
+;; lsp optimization
+(setq lsp-optimization-mode t)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
