fd67c9264b53a71f4ca6268901d8e7b1f63693f8
Dave Nguyen <dv@dvnguyen.com>
2022-01-20 18:20:45 -0800

    Temporarily disable org-directory. Not all my machines have Dropbox directory.

--- config.el
+++ config.el
@@ -29,8 +29,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Dropbox/org")
-(setq org-agenda-files (directory-files-recursively "~/Dropbox/org/" "\\.org$"))
+;; (setq org-directory "~/Dropbox/org")
+;; (setq org-agenda-files (directory-files-recursively "~/Dropbox/org/" "\\.org$"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
